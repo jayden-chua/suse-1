0ee92c5179c88e8492daa2b2f8750fafc8acb722
Jayden Chua <gjiehs@gmail.com>
2021-07-07 15:14:47 +0800

    add api endpoints

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,28 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/status")
+def status():
+    response = {}
+    response['status'] = 200
+    response['data'] = {
+        'result': 'OK - healthy'
+    } 
+    return jsonify(response)
+
+@app.route("/metrics")
+def metrics():
+    response = {}
+    response['status'] = 200
+    response['data'] = {
+        'UserCount': 140,
+        'UserCountActive': 23
+    } 
+    return jsonify(response)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
